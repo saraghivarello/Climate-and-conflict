28261f31f4560d4419470251a8d441edc0f1eeea
saraghivarello <sara.ghivarello@studenti.polito.it>
2024-01-10 10:30:25 +0100

    10-1

---
+++ R/all_regressions copy 2.r
@@ -0,0 +1,34 @@
+library(stargazer)
+library(pspatreg)
+library(spatialreg)
+library(spdep)
+library(sf)
+library(plm)
+library(ggplot2)
+library(dplyr)
+library(splm)
+library(rgdal)
+library(car)
+
+data_confl <- read.csv("/home/sara/Documenti/GitHub/Climate-and-conflict/df_flood.csv")
+
+
+form_lmh <- Disp_log ~ x + TA_lag2 + PA_lag2 + DL_lag2 + inv_distance + conflicts + gdp_mean_origin  + gdp_mean_destination + population_density_origin + population_density_destination #+ accessibility_to_cities_mean_origin + accessibility_to_cities_mean_destination#+ PA_lag3 + DL_lag3 + inv_distance
+form <- Disp_log ~ x + TA_lag2 + PA_lag2 + DL_lag2 + inv_distance_2 + conflicts  +gdp_mean_origin  + gdp_mean_destination + population_density_origin + population_density_destination
+form_1 <- Disp_log ~ x + TA_lag2 + PA_lag2 + DL_lag2 + inv_distance + inv_distance_2 + conflicts  + gdp_mean_origin + gdp_mean_destination + population_density_origin + population_density_destination
+f_plm <- Disp_log ~ x + TA_lag2 + PA_lag2 + DL_lag2 + inv_distance + conflicts 
+lmh <- lm(form_lmh, data = data_confl)
+#stargazer(lmh, type = "text", out = "lmh_log.txt")
+
+lmh_log <- lm(form_lmh, data = data_confl)
+lmh_log1 <- lm(form, data = data_confl)
+lmh_log2 <- lm(form_1, data = data_confl)
+#stargazer(lmh_log, lmh_log1, lmh_log2, type = "latex", out = "lmh_log_latex.tex")
+
+
+#all regions log
+log_ind <- plm(f_plm, data = data_confl, model = "within", index = c("Current..Arrival..Region","month"), effect = "individual")
+log_time <- plm(f_plm, data = data_confl, model = "within", index = c("Current..Arrival..Region","month"), effect = "time")
+log_two <- plm(f_plm, data = data_confl, model = "within", index = c("Current..Arrival..Region","month"), effect = "twoways")
+stargazer(log_ind, log_time, log_two, type = "text", out = "plm_log_latex.tex")
+
--- R/all_regressions copy.r
+++ R/all_regressions copy.r
@@ -14,21 +14,20 @@ data_confl <- read.csv("/home/sara/Documenti/GitHub/Climate-and-conflict/df_conf
 
 
 form_lmh <- Disp_log ~ x + TA_lag2 + PA_lag2 + DL_lag2 + inv_distance + conflicts + gdp_mean_origin  + gdp_mean_destination + population_density_origin + population_density_destination #+ accessibility_to_cities_mean_origin + accessibility_to_cities_mean_destination#+ PA_lag3 + DL_lag3 + inv_distance
-form <- Disp_log ~ x + TA_lag2 + PA_lag2 + DL_lag2 + inv_distance + conflicts  #+gdp_mean_origin  + gdp_mean_destination 
-form_1 <- Disp_log ~ x + TA_lag2 + PA_lag2 + DL_lag2 + inv_distance_2 + conflicts  +gdp_mean_origin + gdp_mean_destination 
-
-#no fixed effects
+form <- Disp_log ~ x + TA_lag2 + PA_lag2 + DL_lag2 + inv_distance_2 + conflicts  +gdp_mean_origin  + gdp_mean_destination + population_density_origin + population_density_destination
+form_1 <- Disp_log ~ x + TA_lag2 + PA_lag2 + DL_lag2 + inv_distance + inv_distance_2 + conflicts  + gdp_mean_origin + gdp_mean_destination + population_density_origin + population_density_destination
+f_plm <- Disp_log ~ x + TA_lag2 + PA_lag2 + DL_lag2 + inv_distance + conflicts 
 lmh <- lm(form_lmh, data = data_confl)
 #stargazer(lmh, type = "text", out = "lmh_log.txt")
 
 lmh_log <- lm(form_lmh, data = data_confl)
 lmh_log1 <- lm(form, data = data_confl)
 lmh_log2 <- lm(form_1, data = data_confl)
-stargazer(lmh_log, lmh_log1, lmh_log2, type = "text", out = "lmh_log_latex.tex")
+#stargazer(lmh_log, lmh_log1, lmh_log2, type = "latex", out = "lmh_log_latex.tex")
 
 
 #all regions log
-log_ind <- plm(form, data = data_confl, model = "within", index = c("Current..Arrival..Region","month"), effect = "individual")
-log_time <- plm(form, data = data_confl, model = "within", index = c("Current..Arrival..Region","month"), effect = "time")
-log_two <- plm(form, data = data_confl, model = "within", index = c("Current..Arrival..Region","month"), effect = "twoways")
-stargazer(log_ind, log_time, log_two, type = "text", out = "plm_log_latex.tex")
+log_ind <- plm(f_plm, data = data_confl, model = "within", index = c("Current..Arrival..Region","month"), effect = "individual")
+log_time <- plm(f_plm, data = data_confl, model = "within", index = c("Current..Arrival..Region","month"), effect = "time")
+log_two <- plm(f_plm, data = data_confl, model = "within", index = c("Current..Arrival..Region","month"), effect = "twoways")
+stargazer(log_ind, log_time, log_two, type = "latex", out = "plm_log_latex.tex")
